7fd08712fdf0c137f3b3eedf2f6d001d0c9e18c9
Vinicius_Vidal <viniciusvrdsilva@gmail.com>
2024-02-22 22:25:44 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function scrollToAbout() {
+    let element = document.getElementById("aboutUs");
+    element.scrollIntoView();
+}
+
+function scrollToContact() {
+    let element = document.getElementById("contactUs");
+    element.scrollIntoView();
+}
